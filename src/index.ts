fa415147a37275573f86b08aa40a279287a9cd0f
Oliverio <oliverio.junior2@gmail.com>
2022-01-31 15:04:54 -0300

    configurações servidor simples

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response, NextFunction} from "express";
+import statusRoute from "./routes/status.routes";
+import usersRoute from "./routes/user.routes";
 
 const app = express();
 
-app.get("/status", (req:Request, res: Response, next: NextFunction )=>{
-    res.status(200).send({ foo: "bar"})
-})
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(usersRoute);
+ 
+app.use(statusRoute);
 
 app.listen(3000, ()=>{
     console.log("Rodando Poha")
